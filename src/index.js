2276df21510f73c8fe6168a44e112de238e28852
AA Mitchell <87596275+Azwa32@users.noreply.github.com>
2024-04-29 15:54:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function handleSourcePress(e){
     // Sends value from the button to analog join 1 in simpl
     // have to first convert the value to int
         const value = parseInt(e.target.value, 10); // get the value of the target, in this case "e"
-        CrComLib.publishEvent("n", "1", value);
+        CrComLib.publishEvent("b", e.target.value, true);
         console.log(e.target.id, "button was pressed!", e.target.value);
         console.log(e)
 
@@ -20,17 +20,36 @@ function handleSourcePress(e){
         Array.from(els).forEach((el) => {
             document.getElementById(el.id).className = "demo";
         });
-    
+
         // now set the source button that was just pressed to active
         document.getElementById(`src${e.target.value}`).className = "demoActive";
-     }
+}
     
-    // listens to the class called sources and runs whenever pressed.  So basically any button inside the div
-    // will trigger this eventListener to run
-    document.querySelector(".sources").addEventListener("click", function (event) {     // when any button with a class of sources is pressed
-        if (event.target.tagName === "BUTTON") {                                        // if event was from a html button                    
-            handleSourcePress(event);
+// listens to the class called sources and runs whenever pressed.  So basically any button inside the div
+// will trigger this eventListener to run
+document.querySelector(".sources").addEventListener("click", function (event) {     // when any button with a class of sources is pressed
+    if (event.target.tagName === "BUTTON") {                                        // if event was from a html button                    
+        handleSourcePress(event);
+    }
+
+    event.stopPropagation();
+});
+
+
+//Touchpanel feedback, Shows error when testing in web browser
+// Digital joins 5 to 8
+for (let i = 5; i <= 8; i++) {
+    CrComLib.subscribeState('b', i, function(value) {
+        // This function will run whenever the current digital join changes.
+        console.log('Digital join ' + i + ' changed to: ' + value);
+
+        // You can add your own logic here to handle the change.
+        // For example, you might want to update the UI to reflect the new state.
+        if (value) {
+            document.getElementById(`resp${i}`).className = 'active';
+        } else {
+            document.getElementById(`resp${i}`).className = 'inactive';
         }
-    
-        event.stopPropagation();
     });
+}
+    //document.getElementById(`resp${e.target.value}`).className = "demoActive";
